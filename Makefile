695923d8c49a2086f52d541b2cdd714b092d84b4
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2024-08-19 14:00:19 +0100

    Initial
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ Makefile
@@ -0,0 +1,38 @@
+Version := $(shell git describe --tags --dirty)
+GitCommit := $(shell git rev-parse HEAD)
+LDFLAGS := "-s -w -X github.com/alexellis/kubetrim/pkg.Version=$(Version) -X github.com/alexellis/kubetrim/pkg.GitCommit=$(GitCommit)"
+PLATFORM := $(shell ./hack/platform-tag.sh)
+SOURCE_DIRS = cmd pkg main.go
+export GO111MODULE=on
+
+.PHONY: all
+all: gofmt test build dist hash
+
+.PHONY: build
+build:
+	go build
+
+.PHONY: gofmt
+gofmt:
+	@test -z $(shell gofmt -l -s $(SOURCE_DIRS) ./ |grep -v vendor/| tee /dev/stderr) || (echo "[WARN] Fix formatting issues with 'make gofmt'" && exit 1)
+
+.PHONY: test
+test:
+	CGO_ENABLED=0 go test $(shell go list ./... | grep -v /vendor/|xargs echo) -cover
+
+.PHONY: e2e
+e2e:
+	CGO_ENABLED=0 go test github.com/alexellis/kubetrim/pkg/get -cover --tags e2e -v
+
+.PHONY: dist
+dist:
+	mkdir -p bin
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags $(LDFLAGS) -o bin/kubetrim
+	CGO_ENABLED=0 GOOS=darwin go build -ldflags $(LDFLAGS) -o bin/kubetrim-darwin
+	CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -a -ldflags $(LDFLAGS) -o bin/kubetrim-darwin-arm64
+	CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags $(LDFLAGS) -o bin/kubetrim-arm64
+	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags $(LDFLAGS) -o bin/kubetrim.exe
+
+.PHONY: hash
+hash:
+	rm -rf bin/*.sha256 && ./hack/hashgen.sh
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/exec"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"  // Import for GCP auth
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" // Import for OIDC (often used with EKS)
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+
+	"k8s.io/client-go/util/homedir"
+)
+
+var (
+	// Version is the version of the tool
+	Version = "dev"
+
+	// GitCommit is the git commit of the tool
+	GitCommit = ""
+
+	writeFile bool
+)
+
+func main() {
+
+	// set usage:
+
+	flag.Usage = func() {
+		fmt.Printf("kubetrim Copyright Alex Ellis (c) 2024\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "kubetrim removes contexts & clusters from your kubeconfig if they are not accessible.\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Set the kubeconfig file to use through the KUBECONFIG environment variable\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "or the default location will be used: ~/.kube/config\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Options:\n")
+
+		flag.PrintDefaults()
+	}
+
+	flag.BoolVar(&writeFile, "write", true, "Write changes to the kubeconfig file")
+	flag.Parse()
+
+	// Load the kubeconfig file
+	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+	if configPath := os.Getenv("KUBECONFIG"); configPath != "" {
+		kubeconfig = configPath
+	}
+
+	// Load the kubeconfig
+	config, err := clientcmd.LoadFromFile(kubeconfig)
+	if err != nil {
+		fmt.Printf("Error loading kubeconfig: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("kubetrim (%s %s) by Alex Ellis \n\nLoaded: %s. Checking..\n", Version, GitCommit, kubeconfig)
+
+	st := time.Now()
+	// List of contexts to be deleted
+	var contextsToDelete []string
+
+	// Enumerate and check all contexts
+	for contextName := range config.Contexts {
+		fmt.Printf("  - %s: ", contextName)
+
+		// Set the context for the current iteration
+		clientConfig := clientcmd.NewNonInteractiveClientConfig(*config, contextName, &clientcmd.ConfigOverrides{}, nil)
+		restConfig, err := clientConfig.ClientConfig()
+		if err != nil {
+			fmt.Printf("Failed to create REST config (%v)\n", err)
+			contextsToDelete = append(contextsToDelete, contextName)
+			continue
+		}
+
+		// Create Kubernetes client
+		clientset, err := kubernetes.NewForConfig(restConfig)
+		if err != nil {
+			fmt.Printf("Failed to create clientset (%v)\n", err)
+			contextsToDelete = append(contextsToDelete, contextName)
+			continue
+		}
+
+		// Check if the context is working
+		err = checkCluster(clientset)
+		if err != nil {
+			fmt.Printf("❌ - (%v)\n", err)
+			contextsToDelete = append(contextsToDelete, contextName)
+		} else {
+			fmt.Println("✅")
+		}
+	}
+
+	// Delete the contexts that are not working
+	for _, contextName := range contextsToDelete {
+		deleteContextAndCluster(config, contextName)
+	}
+
+	if writeFile {
+		// Save the modified kubeconfig
+		if err = clientcmd.WriteToFile(*config, kubeconfig); err != nil {
+			fmt.Printf("Error saving updated kubeconfig: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Updated: %s (in %s).\n", kubeconfig, time.Since(st).Round(time.Millisecond))
+	}
+}
+
+// checkCluster tries to list nodes in the cluster to verify if the context is working
+func checkCluster(clientset *kubernetes.Clientset) error {
+	_, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to connect to cluster: %v", err)
+	}
+	return nil
+}
+
+// deleteContextAndCluster removes the context and its associated cluster from the config
+func deleteContextAndCluster(config *clientcmdapi.Config, contextName string) {
+
+	// Get the cluster name associated with the context
+	clusterName := config.Contexts[contextName].Cluster
+
+	// Delete the context
+	delete(config.Contexts, contextName)
+
+	// Check if any other contexts use this cluster
+	clusterUsed := false
+	for _, ctx := range config.Contexts {
+		if ctx.Cluster == clusterName {
+			clusterUsed = true
+			break
+		}
+	}
+
+	// If the cluster is not used by any other context, delete it
+	if !clusterUsed {
+		delete(config.Clusters, clusterName)
+	}
+}
